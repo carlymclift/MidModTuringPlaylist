6da29ba15ec70a22c5a23b7cb893a9710ea4a096
Carly Clift <carlymclift@gmail.com>
2020-09-02 09:34:50 -0600

    Inital commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import SongController from '../SongController/SongController';
+import { getAllSongs } from '../FetchAPI/apiCalls'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      songs: [],
       songQueue: []
     }
   }
 
+  async componentDidMount() {
+    try {
+      const data = await getAllSongs()
+      console.log(data)
+      this.setState({ songs: data.movies })
+    } catch (error) {
+      this.setState({error: error})
+    }
+  }
 
   render() {
     return (
