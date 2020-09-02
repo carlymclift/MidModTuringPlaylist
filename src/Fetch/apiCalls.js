9495b3be79e198b117c84bd630b7eb79dfc47b53
Carly Clift <carlymclift@gmail.com>
2020-09-02 12:03:42 -0600

    Set up post request

--- src/Fetch/apiCalls.js
+++ src/Fetch/apiCalls.js
@@ -3,3 +3,20 @@ export const getAllSongs = async () => {
     const data = await response.json()
     return data
 }
+
+export const addMovieRating = async (songName, artistName, link) => {
+    const response = await fetch(`http://localhost:8080/api/v1/playlist`, {
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": JSON.stringify({
+          "songName": songName,
+          "artistName": artistName,
+          "link": link
+        })
+      }
+    )
+    const message = await response.json()
+    return message
+  }
